d9bb0f5afe3799678f56a48733e61363276eff5a
Михаил <134938010+JatyPruducts@users.noreply.github.com>
2024-12-05 17:03:11 +0300

    Backend_update

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
 from fastapi import FastAPI, HTTPException
-from typing import Optional
+from typing import Optional, List, Dict
+from pydantic import BaseModel
 
 # uvicorn main:app --reload
 app = FastAPI()
 
 
-@app.get("/")
-async def home() -> dict[str, str]:
-    return {"data": "message"}
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
 
 
-@app.get("/contacts")
-async def contacts() -> int:
-    return 34
+class Post(BaseModel):
+    id: int
+    title: str
+    body: str
+    author: User
 
+
+users = [
+    {"id": 1, "name": "John", "age": 30},
+    {"id": 2, "name": "Jane", "age": 25},
+    {"id": 3, "name": "Bob", "age": 40},
+]
 
 posts = [
-    {"id": 1, "title": "News_1", "body": "Text_1"},
-    {"id": 2, "title": "News_2", "body": "Text_2"},
-    {"id": 3, "title": "News_3", "body": "Text_3"},
+    {"id": 1, "title": "News_1", "body": "Text_1", "author": users[1]},
+    {"id": 2, "title": "News_2", "body": "Text_2", "author": users[0]},
+    {"id": 3, "title": "News_3", "body": "Text_3", "author": users[2]},
 ]
 
 
 @app.get("/items")
-async def items() -> list:
-    return posts
+async def items() -> List[Post]:
+    return [Post(**post) for post in posts]
 
 
 @app.get("/items/{id}")
-async def items_id(id: int = 1) -> dict:
+async def items_id(id: int) -> Post:
     for post in posts:
         if post["id"] == id:
-            return post
+            return Post(**post)
     raise HTTPException(status_code=404, detail="Item not found")  # вывод ошибки
 
 
 @app.get("/search")
-async def search(post_id: Optional[int] = None, post_title: Optional[str] = None) -> dict:
+async def search(post_id: Optional[int] = None, post_title: Optional[str] = None) -> Dict[str, Optional[Post]]:
     # http://127.0.0.1:8000/search?post_id=1&post_title=News_1
     if post_id and post_title:
         for post in posts:
             if post["id"] == post_id and post["title"] == post_title:
-                return post
+                return {"data": Post(**post)}
         raise HTTPException(status_code=404, detail="Post not found")
     else:
-        return {"data": "No post_id provided"}
+        return {"data": None}
