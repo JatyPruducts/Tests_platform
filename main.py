80129b6f28ab3c89d81b6cce77ece8bbbbdd6d49
Михаил <134938010+JatyPruducts@users.noreply.github.com>
2024-12-05 17:38:00 +0300

    Backend_update

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class PostCreate(BaseModel):
+    title: str
+    body: str
+    author_id: int
+
+
 class User(BaseModel):
     id: int
     name: str
@@ -35,6 +41,18 @@ posts = [
 @app.get("/items")
 async def items() -> List[Post]:
     return [Post(**post) for post in posts]
+
+
+@app.post("/items/add")
+async def add_item(post: PostCreate) -> Post:
+    author = next((user for user in users if user["id"] == post.author_id), None)
+    if not author:
+        raise HTTPException(status_code=404, detail="User not found")
+    new_post_id = len(posts) + 1
+    new_post = {"id": new_post_id, "title": post.title, "body": post.body, "author": author}
+    posts.append(new_post)
+
+    return Post(**new_post)
 
 
 @app.get("/items/{id}")
