344386fab8bf25f81f0cf7c8125205c5db2ada5d
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-06 12:04:28 +0300

    Создана проверка на непустые значения

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,21 +2,40 @@
   <div class="bg-white position-absolute mx-auto top-50 start-50 translate-middle 
   border border-opacity-25 p-3 shadow rounded" style="height: 50vb; width: 60vb;">
     <h2>Вход в аккаунт</h2>
-    <b-form class="w-75 mx-auto border border-dark rounded border-opacity-25 p-3 shadow">
-      <b-form-group id="login" label="Логин:" label-for="input-1" class="text-start" description="">
-        <b-form-input id="input-1" type="text" placeholder="Введите логин"></b-form-input>
+    <b-form @submit="onSubmit" class="w-75 mx-auto border border-dark rounded border-opacity-25 p-3 shadow">
+      <b-form-group id="login" label="Логин:" label-for="input-1" class="text-start">
+        <b-form-input id="input-1" type="text" placeholder="Введите логин" v-model="form.login"></b-form-input>
       </b-form-group>
-      <b-form-group id="password" label="Пароль:" label-for="input-2" class="text-start" description="">
-        <b-form-input id="input-2" type="password" placeholder="Введите пароль"></b-form-input>
+      <b-form-group id="password" label="Пароль:" label-for="input-2" class="text-start">
+        <b-form-input id="input-2" type="password" placeholder="Введите пароль" v-model="form.password"></b-form-input>
       </b-form-group>
-      <b-button variant="success">Войти</b-button>
+      <b-button type="submit" variant="primary">Войти</b-button>
     </b-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  data(){
+    return{
+      form:{
+        login:'',
+        password:''
+      }
+    }
+  },
+  methods: {
+    onSubmit(event)
+    {
+      event.preventDefault();
+      if (this.form.login == "") return document.getElementById("input-1").classList.add('is-invalid');
+      document.getElementById("input-1").classList.remove('is-invalid');
+      if (this.form.password == "") return document.getElementById("input-2").classList.add('is-invalid');
+      document.getElementById("input-2").classList.remove('is-invalid');
+      alert(JSON.stringify(this.form));
+    }
+  }
 };
 </script>
 
