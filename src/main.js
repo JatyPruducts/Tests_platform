1fb2b280e0d32b0eecca73b7f3299d25a36e0062
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-06 06:53:45 +0300

    Подключена библиотека boostrap-vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import PortalVue from 'portal-vue';
+
+const app = createApp(App);
+app.use(BootstrapVue3)
+app.use(PortalVue)
+
+app.mount('#app')
