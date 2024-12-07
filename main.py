f16b870dfe90252e4f1a375d9b0fe5f91d8c5340
Михаил <134938010+JatyPruducts@users.noreply.github.com>
2024-12-07 06:21:41 +0300

    Update backend and work with vue.js in App.vue and AuthComponent.vue files.

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI, HTTPException, Path, Query, Body, Depends
-from typing import Optional, List, Dict, Annotated
+from typing import Optional, List, Annotated
 from sqlalchemy.orm import Session
 
+from fastapi.middleware.cors import CORSMiddleware
 from models import Base, User, Post
 from database import session_local, engine
 from schemas import PostCreate, UserCreate, PostResponse, User as DbUser
 
 # uvicorn main:app --reload
 app = FastAPI()
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
@@ -32,7 +45,7 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)) -> DbUser
 
 
 @app.post("/posts/", response_model=PostResponse)  # указываем с какой моделью мы взаимодействуем
-async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> PostResponse:
+async def create_post(post: Annotated[PostCreate, Body(title="Структура нового поста", min_length=3, max_length=500)], db: Session = Depends(get_db)) -> PostResponse:
     db_user = db.query(User).filter(User.id == post.author_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -48,3 +61,11 @@ async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> PostRe
 @app.get("/posts/", response_model=List[PostResponse])
 async def posts(db: Session = Depends(get_db)):
     return db.query(Post).all()
+
+
+@app.get("/users/{name}", response_model=DbUser)
+async def posts(name: str, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.name == name).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
--- public/src/App.vue
+++ public/src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <auth-component v-if="!isAuth" @handleAuth="handleAuth"/>
+    <div v-else>
+      <p>Вы авторизованы</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AuthComponent from './components/AuthComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AuthComponent
+  },
+  data() {
+    return{
+      isAuth: false
+    }
+  },
+  methods:{
+    handleAuth( ){
+      this.isAuth = true;
+    }
   }
 }
 </script>
