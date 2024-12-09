566458a7b6ecfd92d71cf620973ecdbfd1918bb2
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-09 08:38:18 +0300

    Добавлена форма для админа

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { BootstrapVue3 } from 'bootstrap-vue-3';
 
-import { BootstrapVue3 } from 'bootstrap-vue-3'
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+// Импортируйте CSS файлы
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import './style.css'
 import router from './router'
-
 import PortalVue from 'portal-vue';
 
 const app = createApp(App);
+
 app.use(BootstrapVue3);
 app.use(PortalVue);
 app.use(router);
-
 app.mount('#app');
