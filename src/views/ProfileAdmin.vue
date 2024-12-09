668f05fa264828ec5879d80a618534cb047d347f
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-09 09:47:07 +0300

    Реализация взятия текста из docx файла и вброс его в спец. поле

--- src/views/ProfileAdmin.vue
+++ src/views/ProfileAdmin.vue
@@ -1,15 +1,19 @@
 <template>
     <header>
-        <div class="offcanvas offcanvas-start"  tabindex="-1" id="Sections" aria-labelledby="SectionsLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="SectionsLabel">Оглавление</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div class="offcanvas offcanvas-start"  tabindex="-1" id="Sections" aria-labelledby="SectionsLabel" style="text-align: left;">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="SectionsLabel">Оглавление</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul>
+                    <li v-for="lecture in lectures" :key="lecture.title">
+                        <a href="#" @click.prevent="loadLecture(lecture.file)">{{ lecture.title }}</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="offcanvas-body">
-            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Mollis posuere sociosqu rhoncus rutrum suscipit pulvinar praesent eu. Congue bibendum parturient commodo ad parturient imperdiet vel etiam auctor. Purus sed nisi platea massa vivamus magnis vehicula varius. Porta ante ullamcorper vel eleifend proin amet quisque. Nascetur porttitor semper aliquet platea viverra inceptos. Eu diam molestie euismod justo vel aptent pulvinar rutrum. Consectetur facilisis pellentesque dapibus adipiscing consectetur conubia. Quis magnis luctus nostra nec litora; potenti mi tempus. Pharetra commodo malesuada ad nisl mattis, sodales integer dictumst. Cursus dictum pellentesque; id finibus neque ac tempor placerat felis. Sagittis vel lorem; urna rhoncus arcu vestibulum. Condimentum sem tristique; eu suscipit lectus sapien. Diam elementum adipiscing leo; ultrices id ut. Orci himenaeos morbi augue bibendum venenatis morbi senectus condimentum. Tristique leo tristique lobortis litora magna eros varius? Commodo dictumst blandit tortor mauris nisi rutrum pulvinar. Lacus nisl sociosqu orci mattis class luctus. Leo condimentum suscipit ornare habitasse maximus penatibus urna. Integer parturient eleifend suspendisse etiam metus risus eget convallis. Phasellus viverra sit sollicitudin ex pulvinar donec platea vestibulum proin. Non tortor neque enim, aliquam urna per varius. Blandit fermentum habitasse faucibus quis nulla potenti dolor. Vitae interdum tempor senectus sapien cubilia leo class hendrerit? Enim libero turpis sociosqu diam mattis iaculis. Semper primis tellus libero torquent primis aliquam nullam nisl etiam. Habitasse augue litora habitasse quam molestie et enim. Malesuada bibendum convallis leo leo congue. Sociosqu suscipit turpis integer inceptos commodo velit iaculis montes eros. Hac massa viverra diam sed ridiculus egestas platea. Dolor sem nam imperdiet tortor odio. Aligula nascetur lobortis lobortis eu justo metus. Aptent purus viverra massa non amet nisi rutrum libero tristique? Hac parturient efficitur ante amet sit pellentesque nulla tincidunt. Sem integer litora vitae semper per maximus. Ipsum habitasse suscipit dolor mauris elementum. Hac imperdiet taciti himenaeos pellentesque, imperdiet torquent.</p> <br>
-        </div>
-        </div>
-        <b-navbar toggleable="lg" type="dark" class="bg-info bg-gradient shadow">
+        <b-navbar toggleable="lg" type="light" class="bg-info bg-gradient shadow">
             <div>
                 <button type="button" class="btn border rounded p-2" data-bs-toggle="offcanvas" data-bs-target="#Sections" aria-controls="Sections" style="background-color: yellow; color: black;">
                     <i class="bi bi-list"> Разделы</i>
@@ -18,7 +22,7 @@
                 <b-nav-item-dropdown class="border rounded p-2" style="background-color: yellow;font-size: large;" right>
                     <template #button-content>
                         <i class='bi bi-person-square'></i> 
-                         <em> Имя</em> <!--заменим на Имя из БД -->
+                         <em> {{ Uname }}</em> <!--заменим на Имя из БД -->
                     </template>
                     <b-dropdown-item href="#">Статистика</b-dropdown-item>
                     <b-dropdown-item href="#">Учителя</b-dropdown-item>
@@ -30,19 +34,41 @@
     <body>
         <b-card>
             <b-card-body style="position:relative; height:85vh; overflow-y:scroll; text-align: left;">
-                <p>{{ text }}</p>
+                <div v-html="text"></div>  <!--текст будем брать из текстового файла-->
             </b-card-body>
         </b-card>
     </body>
 </template>
 
 <script>
+import mammoth from 'mammoth';
 
 export default {
     name: 'ProfileAdmin',
     data() {
       return {
-        text: "Lorem ipsum odor amet, consectetuer adipiscing elit. Mollis posuere sociosqu rhoncus rutrum suscipit pulvinar praesent eu. Congue bibendum parturient commodo ad parturient imperdiet vel etiam auctor. Purus sed nisi platea massa vivamus magnis vehicula varius. Porta ante ullamcorper vel eleifend proin amet quisque. Nascetur porttitor semper aliquet platea viverra inceptos. Eu diam molestie euismod justo vel aptent pulvinar rutrum. Consectetur facilisis pellentesque dapibus adipiscing consectetur conubia. Quis magnis luctus nostra nec litora; potenti mi tempus. Pharetra commodo malesuada ad nisl mattis, sodales integer dictumst. Cursus dictum pellentesque; id finibus neque ac tempor placerat felis. Sagittis vel lorem; urna rhoncus arcu vestibulum. Condimentum sem tristique; eu suscipit lectus sapien. Diam elementum adipiscing leo; ultrices id ut. Orci himenaeos morbi augue bibendum venenatis morbi senectus condimentum. Tristique leo tristique lobortis litora magna eros varius? Commodo dictumst blandit tortor mauris nisi rutrum pulvinar. Lacus nisl sociosqu orci mattis class luctus. Leo condimentum suscipit ornare habitasse maximus penatibus urna. Integer parturient eleifend suspendisse etiam metus risus eget convallis. Phasellus viverra sit sollicitudin ex pulvinar donec platea vestibulum proin. Non tortor neque enim, aliquam urna per varius. Blandit fermentum habitasse faucibus quis nulla potenti dolor. Vitae interdum tempor senectus sapien cubilia leo class hendrerit? Enim libero turpis sociosqu diam mattis iaculis. Semper primis tellus libero torquent primis aliquam nullam nisl etiam. Habitasse augue litora habitasse quam molestie et enim. Malesuada bibendum convallis leo leo congue. Sociosqu suscipit turpis integer inceptos commodo velit iaculis montes eros. Hac massa viverra diam sed ridiculus egestas platea. Dolor sem nam imperdiet tortor odio. Aligula nascetur lobortis lobortis eu justo metus. Aptent purus viverra massa non amet nisi rutrum libero tristique? Hac parturient efficitur ante amet sit pellentesque nulla tincidunt. Sem integer litora vitae semper per maximus. Ipsum habitasse suscipit dolor mauris elementum. Hac imperdiet taciti himenaeos pellentesque, imperdiet torquent. Vivamus tempor imperdiet ultricies maximus rutrum."
+        Uname: "Иван",
+        text: "",
+        lectures: [
+            {title: "Лекция 1", file: "/lectures/SomeText.docx"},
+            {title: "Лекция 2", file: "/lectures/Lecture2.docx"}
+        ]
+        };
+    },
+    methods: {
+        async loadLecture(file) {
+            try {
+                const response = await fetch(file);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const arrayBuffer = await response.arrayBuffer(); // Получаем массив байтов
+                const { value: html } = await mammoth.convertToHtml({ arrayBuffer }); // Конвертируем в HTML
+                this.text = html; // Сохраняем HTML для отображения
+            } catch (error) {
+                console.error('Error fetching the lecture:', error);
+            }
         }
     }
 }
