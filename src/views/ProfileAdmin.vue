978902c5c35987b86703b42c13a689573a67898b
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-09 21:11:29 +0300

    Добавлены кнопки навигации

--- src/views/ProfileAdmin.vue
+++ src/views/ProfileAdmin.vue
@@ -2,13 +2,17 @@
     <header>
         <div class="offcanvas offcanvas-start"  tabindex="-1" id="Sections" aria-labelledby="SectionsLabel" style="text-align: left;">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="SectionsLabel">Оглавление</h5>
+                <h3 class="offcanvas-title" id="SectionsLabel">Оглавление</h3>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
+            <div class="offcanvas-body border" style="font-size: large;">
                 <ul>
                     <li v-for="lecture in lectures" :key="lecture.title">
-                        <a href="#" @click.prevent="loadLecture(lecture.title, lecture.file)">{{ lecture.title }}</a>
+                        <a href="#" 
+                        @click.prevent="loadLecture(lecture.title, lecture.file)"
+                        :class="{ 'active-lecture': lecture.title === lectureTitle }"
+                        >
+                        {{ lecture.title }}</a>
                     </li>
                 </ul>
             </div>
@@ -33,10 +37,14 @@
     </header>
     <body>
         <b-card>
-            <b-card-body style="position:relative; height:85vh; overflow-y:scroll; text-align: left;">
+            <b-card-body style="position:relative; height:80vh; overflow-y:scroll; text-align: left; font-size: large;">
                 <h1>{{ lectureTitle }}</h1>
                 <div v-html="text"></div>  <!--текст будем брать из текстового файла-->
             </b-card-body>
+            <b-card-footer>
+                <b-button @click="prevLecture" :disabled="currentLectureIndex === 0">Предыдущая лекция</b-button>
+                <b-button @click="nextLecture" :disabled="currentLectureIndex === lectures.length - 1">Следующая лекция</b-button>
+            </b-card-footer>
         </b-card>
     </body>
 </template>
@@ -51,15 +59,23 @@ export default {
         Uname: "Иван",
         text: "",
         lectureTitle: "",
+        currentLectureIndex: 0, // Индекс текущей лекции
         lectures: [
             {title: "Лекция 1", file: "/lectures/SomeText.docx"},
             {title: "Лекция 2", file: "/lectures/Lecture2.docx"}
         ]
         };
     },
+    mounted() {
+        // Загружаем первую лекцию при монтировании компонента
+        if (this.lectures.length > 0) {
+            this.loadLecture(this.lectures[0].title, this.lectures[0].file);
+        }
+    },
     methods: {
         async loadLecture(title, file) {
             this.lectureTitle = title; // Устанавливаем заголовок лекции из массива
+            this.currentLectureIndex = this.lectures.findIndex(lecture => lecture.title === title); 
             try {
                 const response = await fetch(file);
                 if (!response.ok) {
@@ -72,7 +88,26 @@ export default {
             } catch (error) {
                 console.error('Error fetching the lecture:', error);
             }
+        },
+        prevLecture() {
+            if (this.currentLectureIndex > 0) {
+                const prevLecture = this.lectures[this.currentLectureIndex - 1];
+                this.loadLecture(prevLecture.title, prevLecture.file);
+            }
+        },
+        nextLecture() {
+            if (this.currentLectureIndex < this.lectures.length - 1) {
+                const nextLecture = this.lectures[this.currentLectureIndex + 1];
+                this.loadLecture(nextLecture.title, nextLecture.file);
+            }
         }
     }
 }
 </script>
+
+<style>
+.active-lecture {
+    font-weight: bold; /* Делает текст жирным */
+    color: blue; /* Изменяет цвет текста, можно выбрать любой */
+}
+</style>
