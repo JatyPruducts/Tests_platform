0c8810b08f7b079fe9d7aa283760558d63c67eb5
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-21 22:39:32 +0300

    Создание форм для учителя и ученика, перенаправление с форм регистрации и входа в зависимости от ролей

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import ProfileAdmin from '../views/ProfileAdmin.vue';
+import ProfileStudent from '../views/ProfileStudent.vue';
+import ProfileTeacher from '../views/ProfileTeacher.vue';
+
 import RegistrationPage from '@/views/RegistrationPage.vue';
 const routes = [
     { path: '/', name:'home', component: HomePage },
-    { path: '/admin/main/', name:'AdminProfile', component: ProfileAdmin},
-    { path: '/registration', name:'registration', component: RegistrationPage }
+    { path: '/admin/main/', name:'ProfileAdmin', component: ProfileAdmin},
+    { path: '/registration', name:'registration', component: RegistrationPage },
+    { path: '/student/main/', name:'ProfileStudent', component: ProfileStudent},
+    { path: '/teacher/main/', name:'ProfileTeacher', component: ProfileTeacher},
 ];
 const router = createRouter({
     history: createWebHistory(), 
