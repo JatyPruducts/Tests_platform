7a0c79c5bb2185debb6137a8921ecc7828aa0a4f
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-12-22 23:22:29 +0300

    Создана таблица с учениками. Теперь при нажатии на кнопку Разделы, не находясь в main, будет переход в main

--- src/views/StudentsList.vue
+++ src/views/StudentsList.vue
@@ -2,13 +2,12 @@
     <header>
         <b-navbar toggleable="lg" type="light" class="bg-info bg-gradient shadow">
             <div>
-                <button type="button" class="btn border rounded p-2" data-bs-toggle="offcanvas" data-bs-target="#Sections" aria-controls="Sections" style="background-color: #f1faa3; color: black;">
+                <b-button type="button" class="btn border rounded p-2" href="/teacher/main/" aria-controls="Sections" style="background-color: #f1faa3; color: black;">
                     <i class="bi bi-list"> Разделы</i>
-                </button>
+                </b-button>
             </div>
-                <b-nav-item-dropdown class="border rounded p-2" style="background-color: #f1faa3;font-size: large;" right>
+                <b-nav-item-dropdown class="border rounded p-2" style="background-color: #f1faa3;font-size: large;" right >
                     <template #button-content>
-                        <i class='bi bi-person-square'></i> 
                         <em> {{ name }}</em> <!--заменим на Имя из БД -->
                     </template>
                     <b-dropdown-item href="/teacher/profile/">Профиль</b-dropdown-item>
@@ -20,14 +19,55 @@
     <body>
         <b-card>
             <b-card-body style="position:relative; height:87vh; overflow-y:scroll; text-align: left; font-size: large;">
-
+                <h1>Ваши ученики:</h1>
+                <b-table :items="students" :fields="fields" striped hover>
+                <template #cell(name)="data">
+                    <strong>{{ data.item.name }}</strong>
+                </template>
+                <template #cell(surname)="data">
+                    <strong>{{ data.item.surname }}</strong>
+                </template>
+                <template #cell(login)="data">
+                    <strong>{{ data.item.login }}</strong>
+                </template>
+                </b-table>
             </b-card-body>
         </b-card>
     </body>
 </template>
 
 <script>
+import axios
+ from 'axios';
 export default {
     name: 'StudentsList',
+    data() {
+      return {
+        name: "",
+        login: '',
+        students: [], // Здесь будут храниться данные о студентах
+        fields: [
+            { key: 'name', label: 'Имя' },
+            { key: 'surname', label: 'Фамилия' },
+            { key: 'login', label: 'Логин' },
+        ],
+        };
+      },
+    async mounted()
+    {
+        try {
+        const userData = localStorage.getItem('user');
+        this.user = JSON.parse(userData);
+        this.name = this.user.name;
+        this.login = this.user.login;
+        const response = await axios.get(`http://127.0.0.1:8000/students_by_teacher/${this.login}`, {
+            teacher_login: this.login
+            }); 
+        this.students = response.data;
+    } catch(error)
+    {
+        alert('Ошибка: ' + error.response.data.detail);
     }
+    },
+}
 </script>
